b63226426070318b6395666561b08c122d1ae481
Rune Laenen <rune.laenen@phpro.be>
2018-12-25 11:11:37 +0100

    port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/:ip\::port(\\d+)/', function (req, res){
   requestGameInfo(res, req.params.ip, req.params.port);
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 
 var requestGameInfo = function requestGameInfo(res, address, port) {
   var netComGI = new Buffer([0, 4, 0, 0, 0, 9]);
